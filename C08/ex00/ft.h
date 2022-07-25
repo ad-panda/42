ba3dc5375df8de74a8d35510e17b83647e2896e1
Adrien Simoes ferreira <asimoes-@z3r5p5.42lyon.fr>
2022-07-25 11:46:40 +0200

    push c08

---
+++ C08/ex00/ft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimoes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/21 19:56:50 by asimoes-          #+#    #+#             */
+/*   Updated: 2022/07/21 20:04:24 by asimoes-         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_H__
+
+#define __FT_H__
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimoes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/21 20:05:18 by asimoes-          #+#    #+#             */
+/*   Updated: 2022/07/24 10:28:40 by asimoes-         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_BOOLEAN_H__
+
+#define __FT_BOOLEAN_H__
+
+#include <unistd.h>
+#define EVEN_MSG  "I have an even number of arguments."
+#define ODD_MSG  "I have an odd number of arguments."
+#define TRUE 1
+#define FALSE 0
+#define SUCCES 0
+#define EVEN(x) x % 2 == 0
+typedef int	t_bool; 
+
+#endif
