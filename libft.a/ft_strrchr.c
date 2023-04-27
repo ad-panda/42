4a27e66cd181e5247d70db4543897a11c4c1536d
ad-panda <108462406+ad-panda@users.noreply.github.com>
2023-04-27 19:34:23 +0200

    uptdate

--- libft.a/ft_strrchr.c
+++ libft.a/ft_strrchr.c
@@ -1,18 +1,19 @@
 #include "libft.h"
 
-char
-	*ft_strrchr(const char *s, int c)
+char    *ft_strrchr(const char *s, int c)
 {
 	int	i;
+	char	*str;
 
-	i = 0;
-	while (s[i])
-		i++;
-	while (i >= 0)
+	i = ft_strlen((char *)s);
+	str = (char *)s;
+	while (str[i] != (char)c && i >= 0)
 	{
-		if (s[i] == (char)c)
-			return ((char*)(s + i));
+		if (str[i - 1] == '\0')
+			return (NULL);
 		i--;
 	}
-	return (NULL);
+        if (str[i] != (char)c)
+                return (NULL);
+	return ((char *)s + i);
 }
