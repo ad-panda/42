307f19a0ba7af25167bf42729f5e695300c1cd9d
ad-panda <108462406+ad-panda@users.noreply.github.com>
2023-06-13 21:10:19 +0200

    restructuration

--- ft_printf/include/ft_printf.h
+++ ft_printf/include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: asimoes- <asimoes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 20:25:43 by asimoes-          #+#    #+#             */
-/*   Updated: 2023/05/21 20:20:55 by asimoes-         ###   ########.fr       */
+/*   Updated: 2023/05/30 12:14:36 by asimoes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PRINTF_H
 
 # include <string.h>
-# include <stdint.h>
+# include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
 
--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -6,9 +6,31 @@
 /*   By: asimoes- <asimoes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 20:24:18 by asimoes-          #+#    #+#             */
-/*   Updated: 2023/05/21 20:25:04 by asimoes-         ###   ########.fr       */
+/*   Updated: 2023/06/10 00:57:31 by asimoes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int    ft_printf(const char *str, ...)
+{
+    va_list ap;
+    va_start(ap, str);
+    int lenght_str;
+    
+    if ( == "%")
+    {
+        if( + 1 == "c")
+            ft_putchar();
+        if( + 1 == "s")
+            ft_putstr();
+        if( + 1 == "d")
+            ft_putdecimal();
+        if( + 1 == "x")
+
+        if( + 1 == "i")
+              
+    }
+    va_end(ap);
+    return (lenght_str);
+}
