30cce2d4d7d0756cd930250dc2b2cb711bd21f5a
ad-panda <108462406+ad-panda@users.noreply.github.com>
2023-11-14 16:30:05 +0100

    error leak

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: asimoes- <asimoes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:30:27 by asimoes-          #+#    #+#             */
-/*   Updated: 2023/11/13 13:24:46 by asimoes-         ###   ########.fr       */
+/*   Updated: 2023/11/14 16:27:09 by asimoes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
-
 char	*ft_new_strstash(char *strstash);
 char	*ft_read_to_strstash(int fd, char *strstash);
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *strstash, char *buff);
 char	*ft_get_the_line(char *strstash);
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: asimoes- <asimoes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:25:17 by asimoes-          #+#    #+#             */
-/*   Updated: 2023/11/13 13:20:39 by asimoes-         ###   ########.fr       */
+/*   Updated: 2023/11/14 15:26:13 by asimoes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,58 +22,50 @@ size_t	ft_strlen(const char *str)
 	return (len);
 }
 
-char	*ft_strchr(const char *s, int c)
-{
-	int		i;
-	char	*str;
-
-	i = 0;
-	str = (char *)s;
-	while (str[i] != (char)c)
-	{
-		if (str[i] == '\0')
-			return (NULL);
-		i++;
-	}
-	return (str + i);
-}
-
-static	char	*ft_strcat(char *dst, char *src)
+char	*ft_strchr(char *s, int c)
 {
 	int	i;
 
 	i = 0;
-	while (src[i])
+	if (!s)
+		return (0);
+	if (c == '\0')
+		return ((char *)&s[ft_strlen(s)]);
+	while (s[i] != '\0')
 	{
-		dst[i] = src[i];
+		if (s[i] == (char) c)
+			return ((char *)&s[i]);
 		i++;
 	}
-	dst[i] = '\0';
-	return (dst);
+	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *strstash, char *buff)
 {
-	int		ls3;
-	int		j;
-	int		len;
-	char	*s3;
+	size_t	i;
+	size_t	j;
+	char	*str;
 
-	j = 0;
-	len = (ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
-	(s3 = malloc(len));
-	if (s3 == NULL)
-		return (NULL);
-	s3 = ft_strcat(s3, (char *)s1);
-	ls3 = ft_strlen(s3);
-	while (s2[j])
+	if (!strstash)
 	{
-		s3[ls3] = s2[j];
-		ls3++;
-		j++;
+		strstash = (char *)malloc(1 * sizeof(char));
+		strstash[0] = '\0';
 	}
-	s3[ls3] = '\0';
-	return (s3);
+	if (!strstash || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(strstash) + ft_strlen(buff)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (strstash)
+		while (strstash[++i] != '\0')
+			str[i] = strstash[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(strstash) + ft_strlen(buff)] = '\0';
+	free(strstash);
+	return (str);
 }
 
 char	*ft_get_the_line(char *strstash)
